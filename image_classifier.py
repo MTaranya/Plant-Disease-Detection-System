3ad3faeeadab1b4a92d26732fe60e48b5b94510e
MTaranya <taranya.m.2024.it@rajalakshmi.edu.in>
2026-06-17 14:52:16 +0530

    Added image classification code

---
+++ image_classifier.py
@@ -0,0 +1,15 @@
+import cv2
+import numpy as np
+
+image = cv2.imread("sample_leaf.jpg")
+
+image = cv2.resize(image, (300, 300))
+
+green = np.mean(image[:, :, 1])
+
+print("Green Value:", green)
+
+if green > 100:
+    print("Healthy Plant")
+else:
+    print("Diseased Plant")
